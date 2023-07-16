0156101b824af07e3a581f7d8291ea597ba28e8a
gurunelee <dlckdgk4858@gmail.com>
2023-07-16 22:12:26 +0900

    github actions setting

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  base: 'cherry1004',
+  lang: 'en-US',
+  title: 'Cherry1004/archive',
+  description: '개인화된 저장소',
+})
